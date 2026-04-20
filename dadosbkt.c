3bcee527401ebeaadedb1a864a5f690f8f0e71fe
Rito-x45 <155210435+Rito-x45@users.noreply.github.com>
2026-04-20 13:30:02 -0600

    backtracking

--- dadosbkt.c
+++ dadosbkt.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
-void simdados(int dado, int sum , int listdad[], int val, int count);
-void simdadosbkt(int d1, int d2, int d3, int val, int count, int *sol);
+void simdados(int dado, int sum , int listdad[], int val, int *count);
+void simdadosbkt(int dado, int sum , int listdad[], int val, int *count);
 
 int main()
 {
-    int val = 0, sol = 0, num_dados = 3;
+    int val = 0, sol = 0, num_dados = 3, count = 1;
     int listdad[num_dados]; 
 
     printf("Ingresa la suma de los dados: ");
     scanf("%d", &val);
 
-    simdados(0, 0, listdad, val, 1);
+    simdadosbkt(0, 0, listdad, val, &count);
+    printf("\n\nTotal de combinaciones encontradas: %d\n", count-1);
+
     return 0;
 
 }
 
-void simdados(int dado, int sum , int listdad[], int val, int count)
+void simdados(int dado, int sum , int listdad[], int val, int *count)
 {
     if(dado == 3)
     {
-        printf("\n[%d] convinacion: %d, %d, %d", count, listdad[0], listdad[2], listdad[3]);
+        printf("\n[%d] convinacion: %d, %d, %d", *count, listdad[0], listdad[2], listdad[3]);
         count ++;
 
         if(sum >= val)
@@ -38,7 +40,31 @@ void simdados(int dado, int sum , int listdad[], int val, int count)
     }
 }
 
-void simdadosbkt(int d1, int d2, int d3, int val, int count, int *sol)
+void simdadosbkt(int dado, int sum, int listdad[], int val, int *count) 
 {
+    int num_total_dados = 3;
+    int dadosrest = num_total_dados - dado;
+    int summax = sum + (dadosrest * 6);
 
+
+    if (summax < val) 
+    {
+        return;
+    }
+
+    if (dado == num_total_dados) {
+        if (sum >= val) {
+            printf("\n[%d] combinacion: %d, %d, %d -> Suma: %d", 
+                    *count, listdad[0], listdad[1], listdad[2], sum);
+            (*count)++; 
+        }
+        return;
+    }
+
+    
+    for (int i = 1; i <= 6; i++) {
+        listdad[dado] = i; 
+        simdadosbkt(dado + 1, sum + i, listdad, val, count); 
+       
+    }
 }
